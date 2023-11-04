2b8acc9fb188e1f6905aacec89cb183462c49eb1
“artur” <molart1974@gmail.com>
2023-11-04 19:40:01 +0100

    shorter code in js

--- script.js
+++ script.js
@@ -11,63 +11,62 @@ const roll = document.querySelector(".btn-roll");
 const result = document.querySelector(".result");
 const mainBtn = document.querySelectorAll(".btn");
 
-const showPop4 = (params) => {
+const showPop = () => {
   popup.style.visibility = "visible";
   result.textContent = "Good Luck";
+  popup.classList.add("animation");
+};
+
+const showPop4 = () => {
+  showPop();
   function k4() {
     let number = Math.floor(Math.random() * 4 + 1);
     result.textContent = number;
   }
   roll.addEventListener("click", k4);
 };
-const showPop6 = (params) => {
-  popup.style.visibility = "visible";
-  result.textContent = "Good Luck";
+const showPop6 = () => {
+  showPop();
   function k6() {
     let number = Math.floor(Math.random() * 6 + 1);
     result.textContent = number;
   }
   roll.addEventListener("click", k6);
 };
-const showPop8 = (params) => {
-  popup.style.visibility = "visible";
-  result.textContent = "Good Luck";
+const showPop8 = () => {
+  showPop();
   function k8() {
     let number = Math.floor(Math.random() * 8 + 1);
     result.textContent = number;
   }
   roll.addEventListener("click", k8);
 };
-const showPop10 = (params) => {
-  popup.style.visibility = "visible";
-  result.textContent = "Good Luck";
+const showPop10 = () => {
+  showPop();
   function k10() {
     let number = Math.floor(Math.random() * 10 + 1);
     result.textContent = number;
   }
   roll.addEventListener("click", k10);
 };
-const showPop12 = (params) => {
-  popup.style.visibility = "visible";
-  result.textContent = "Good Luck";
+const showPop12 = () => {
+  showPop();
   function k12() {
     let number = Math.floor(Math.random() * 12 + 1);
     result.textContent = number;
   }
   roll.addEventListener("click", k12);
 };
-const showPop20 = (params) => {
-  popup.style.visibility = "visible";
-  result.textContent = "Good Luck";
+const showPop20 = () => {
+  showPop();
   function k20() {
     let number = Math.floor(Math.random() * 20 + 1);
     result.textContent = number;
   }
   roll.addEventListener("click", k20);
 };
-const showPop100 = (params) => {
-  popup.style.visibility = "visible";
-  result.textContent = "Good Luck";
+const showPop100 = () => {
+  showPop();
   function k100() {
     let number = Math.floor(Math.random() * 100 + 1);
     result.textContent = number;
@@ -75,9 +74,10 @@ const showPop100 = (params) => {
   roll.addEventListener("click", k100);
 };
 
-const hidPop = (params) => {
+const hidePop = () => {
   popup.style.visibility = "hidden";
   result.textContent = "";
+  popup.classList.remove("animation");
 };
 
 btn4.addEventListener("click", showPop4);
@@ -87,4 +87,4 @@ btn10.addEventListener("click", showPop10);
 btn12.addEventListener("click", showPop12);
 btn20.addEventListener("click", showPop20);
 btn100.addEventListener("click", showPop100);
-returnBtn.addEventListener("click", hidPop);
+returnBtn.addEventListener("click", hidePop);
